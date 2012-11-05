015c02b415012a59ac8ab89805d2b344f00ef0a7
Simao Belchior de Castro <simaobelchior@gmail.com>
2012-11-05 10:04:10 +0000

    Created Rails API app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Magpie::Application.config.secret_token = '2fa58cd9858b3d19b6babebe1d545393d8be47cd6d129e369d0e78e6d09d993b23791726dc18df5efef5be9e6cebba571d67c989f74d8622c26b0e517fe023ae'
