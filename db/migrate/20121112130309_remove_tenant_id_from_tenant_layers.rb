a70f88954832e41ec9d8c32ef3762929a8d4fd16
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2012-11-14 12:58:18 +0000

    merged migrations in order to get rid of the json migration

--- db/migrate/20121112130309_remove_tenant_id_from_tenant_layers.rb
+++
@@ -1,9 +0,0 @@
-class RemoveTenantIdFromTenantLayers < ActiveRecord::Migration
-  def up
-    remove_column :tenant_layers, :tenant_id
-  end
-
-  def down
-    add_column :tenant_layers, :tenant_id, :integer
-  end
-end
--- db/migrate/20121112150018_change_analysis_id_to_area_id_in_polygons.rb
+++
@@ -1,9 +0,0 @@
-class ChangeAnalysisIdToAreaIdInPolygons < ActiveRecord::Migration
-  def up
-    rename_column :polygons, :analysis_id, :area_id
-  end
-
-  def down
-    rename_column :polygons, :area_id, :analysis_id
-  end
-end
