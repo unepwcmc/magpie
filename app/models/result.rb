73918e1665957f3cc59d5eaa5aa7a94b3d354369
Simao Belchior de Castro <simaobelchior@gmail.com>
2012-11-15 14:28:40 +0000

    renamed calculation_result_spec to result_spec

--- app/models/result.rb
+++ app/models/result.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: results
+#
+#  id         :integer          not null, primary key
+#  value      :float
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Result < ActiveRecord::Base
   attr_accessible :value
 end
