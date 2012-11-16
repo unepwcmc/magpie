75bbe32fed134de23e6884688dfb710ba36b452e
Simao Belchior de Castro <simaobelchior@gmail.com>
2012-11-16 12:35:49 +0000

    merged branch

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121115180841) do
+ActiveRecord::Schema.define(:version => 20121116100346) do
 
   create_table "app_layers", :force => true do |t|
     t.string   "display_name",                   :null => false
