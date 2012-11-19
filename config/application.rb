5b825b03062846be73cb7a66b00cc1fa38a3d262
Simao Belchior de Castro <simaobelchior@gmail.com>
2012-11-19 10:39:46 +0000

    code had some strange characters

--- config/application.rb
+++ config/application.rb
@@ -68,10 +68,10 @@ module Magpie
       end
     }
     config.middleware.use Rack::Cors do
-         allow do
-             origins '*'
-             resource '*', :headers => :any, :methods => [:get, :post, :options]
-           end
-       end
+      allow do
+        origins "*"
+        resource "*", :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
   end
 end
