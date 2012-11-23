4ceb557626cca93c400a008c570d4fbc057976d0
Simao Belchior de Castro <simaobelchior@gmail.com>
2012-11-23 16:30:03 +0000

    Updated Apps new page.

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -16,9 +16,15 @@ class AppsController < ApplicationController
   def create
     @app = App.new(params[:app])
     if @app.save && @app.use
-      params[:rasters].each do |l|
-        id, name = l.split(',')
-        AppLayer.create(:type => "RasterLayer", :provider_id => id, :display_name => name, :tile_url => "derp")
+      params[:layers].each do |layer_id, options|
+        break if options["include"] != "1"
+        layer = AppLayer.create(:type => "RasterLayer", :provider_id => layer_id, :display_name => options["display_name"], :is_displayed => (options.has_key?("display") && options["display"] == "1"),  :tile_url => "derp")
+        options["operations"].each do |key, h|
+          operation = Operation.find_or_create_by_name(key.downcase)
+          if h.has_key?("include") && h["include"] == "1"
+            layer.calculations.create(:display_name => h["display_name"], :unit => h["unit"])
+          end
+        end
       end
       request.headers['HTTP_X_MAGPIE_APPID'] = @app.id
       render :create
