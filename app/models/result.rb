f923268095fe123323314f3f790f6a39374fd866
Adam Mulligan <adammulligan@gmail.com>
2013-01-06 20:40:23 +0000

    Fix for raster-stats URL

--- app/models/result.rb
+++ app/models/result.rb
@@ -6,7 +6,7 @@ class Result < ActiveRecord::Base
 
   def fetch
     if area_of_interest.polygons.count > 0
-      response = RestClient.get("http://raster-stats.unep-wcmc.org/rasters/#{calculation.project_layer.provider_id}/operations/#{calculation.operation.name}", params: {polygon: area_of_interest.polygons_as_geo_json})
+      response = RestClient.get("http://raster-stats.unep-wcmc.org/rasters/#{calculation.project_layer.provider_id}/operations/#{calculation.operation}", params: {polygon: area_of_interest.polygons_as_geo_json})
       response_json = JSON.parse(response)
       self.value = response_json["value"].to_f
       save
