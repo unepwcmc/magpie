dbafe3f249d6b5f50bf729600728ddd3aa9569db
Décio Ferreira <decio.ferreira@decioferreira.com>
2013-01-15 12:25:51 +0000

    Move fetch result logic to ProjectLayer subclasses

--- app/models/raster_layer.rb
+++ app/models/raster_layer.rb
@@ -10,4 +10,14 @@ class RasterLayer < ProjectLayer
   def operations_select
     JSON.parse(RestClient.get('http://raster-stats.unep-wcmc.org/operations.json')).map { |r| [r['display_name'], r['name']] }
   end
+
+  def fetch_result(result)
+    if result.area_of_interest.polygons.count > 0
+      calc, aoi = result.calculation, result.area_of_interest
+
+      response = RestClient.get("http://raster-stats.unep-wcmc.org/rasters/#{calc.project_layer.provider_id}/operations/#{calc.operation}", params: {polygon: aoi.polygons_as_geo_json})
+      response_json = JSON.parse(response)
+      return response_json["value"].to_f
+    end
+  end
 end
