6c5daed854b3f533d5c6e521715c0aecee19d00e
Décio Ferreira <decio.ferreira@decioferreira.com>
2013-01-16 13:43:16 +0000

    Add different types of results

--- app/models/protected_planet_layer.rb
+++ app/models/protected_planet_layer.rb
@@ -8,16 +8,44 @@ class ProtectedPlanetLayer < ProjectLayer
   end
 
   def operations_select
-    [['Protected area KM2', 'area_protected_km2']]
+    operations = []
+    AVAILABLE_OPERATIONS.each { |k,v| operations << [v[:name], k] }
+    return operations
   end
 
   def fetch_result(result)
-    if result.area_of_interest.polygons.count > 0
-      aoi = result.area_of_interest
+    aoi = result.area_of_interest
 
+    if aoi.polygons.count > 0
       response = RestClient.post("http://protectedplanet.net/api2/geo_searches", "data=#{aoi.to_wkt.to_json}")
       response_json = JSON.parse(response)
-      return response_json["sum_pa_cover_km2"].to_f
+
+      operation = self.class.result_class(result.calculation.operation)
+      operation.fetch(response_json)
     end
   end
+
+  def self.result_class(id)
+    AVAILABLE_OPERATIONS[id.to_sym].result_class
+  end
+
+  private
+
+  AVAILABLE_OPERATIONS = {
+    number_protected_areas: {
+      name: 'Number of Protected Areas',
+      result_class: FloatResult,
+      fetch: lambda { |response| return 7 }
+    },
+    sum_pa_cover_km2: {
+      name: 'AOI within Protected Area (km)',
+      result_class: FloatResult,
+      fetch: lambda { |response| return response["sum_pa_cover_km2"].to_f }
+    },
+    protected_areas_details: {
+      name: 'Protected Areas Details',
+      result_class: JsonResult,
+      fetch: lambda { |response| return {} }
+    }
+  }
 end
