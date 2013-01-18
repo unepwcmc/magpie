2f230c72e4da36119a0c6708f0e37e436b24dd1b
Décio Ferreira <decio.ferreira@decioferreira.com>
2013-01-18 14:05:09 +0000

    CSV download

--- app/models/area_of_interest.rb
+++ app/models/area_of_interest.rb
@@ -25,4 +25,43 @@ class AreaOfInterest < ActiveRecord::Base
   def to_wkt
     polygons.map(&:to_wkt)
   end
+
+  def to_csv(options = {})
+    CSV.generate(options) do |csv|
+      headers, values = [], []
+
+      FloatResult.find_all_by_area_of_interest_id(id).each do |result|
+        headers << result.calculation.display_name
+        values << "#{result.value}#{result.calculation.unit}"
+      end
+
+      csv << headers
+      csv << values
+
+      protected_planet_headers, protected_planet_values = ['WDPA Site Code', 'Area', 'Designation', 'Area (km2)'], []
+
+      JsonResult.find_all_by_area_of_interest_id(id).each do |result|
+        if result.calculation.project_layer.class == ProtectedPlanetLayer
+          JSON.parse(result.value_json).each do |protected_planet_result|
+            protected_planet_result['protected_areas'].each do |protected_area|
+              protected_planet_values << [
+                protected_area['wdpaid'],
+                protected_area['name'],
+                protected_area['data_standard']['DESIG'],
+                protected_area['query_area_protected_km2']
+              ]
+            end
+          end
+        end
+      end
+
+      unless protected_planet_values.empty?
+        csv << [] # Add empty line
+        csv << protected_planet_headers
+        protected_planet_values.each do |protected_planet_value|
+          csv << protected_planet_value
+        end
+      end
+    end
+  end
 end
