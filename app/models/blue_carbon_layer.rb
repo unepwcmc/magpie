24e443ad1f0ee6ba50b4796243939bf4bd3181a2
James Cox <th3james@gmail.com>
2013-04-03 13:20:24 +0100

    Add get_providers to BlueCarbonLayer

--- app/models/blue_carbon_layer.rb
+++ app/models/blue_carbon_layer.rb
@@ -1,4 +1,24 @@
 class BlueCarbonLayer < ProjectLayer
+  PROVIDER_LAYERS = {
+    'mangrove' => 'Mangrove',
+    'seagrass' => 'Seagrass',
+    'sabkha' => 'Sabkha',
+    'saltmarsh' => 'Salt Marsh',
+    'algal_mat' => 'Algal Mat',
+    'other' => 'Other'
+  }
+
+  def self.get_providers
+    providers = []
+    PROVIDER_LAYERS.each do |name, display_name|
+      providers.push({
+        display_name: display_name, 
+        tiles_url_format: "https://carbon-tool.cartodb.com/tiles/bc_#{name}/{z}/{x}/{y}.png?sql=SELECT * FROM bc_#{name} WHERE toggle = true AND (action <> \'delete\' OR action IS NULL)"
+      })
+    end
+    return providers
+  end
+
   def self.get_operations
     return [{
         name: "sum",
--- spec/models/blue_carbon_layer_spec.rb
+++ spec/models/blue_carbon_layer_spec.rb
@@ -2,8 +2,18 @@ require 'spec_helper'
 
 describe BlueCarbonLayer do
   describe "#get_providers" do
-    it "should return layer providers" do
-      pending
+    before :all do
+      @result = BlueCarbonLayer.get_providers
+    end
+    it "returns the blue carbon cartodb layers" do
+      @result.should match_array([
+        {display_name: 'Mangrove', tiles_url_format: 'https://carbon-tool.cartodb.com/tiles/bc_mangrove/{z}/{x}/{y}.png?sql=SELECT * FROM bc_mangrove WHERE toggle = true AND (action <> \'delete\' OR action IS NULL)'},
+        {display_name: 'Seagrass', tiles_url_format: 'https://carbon-tool.cartodb.com/tiles/bc_seagrass/{z}/{x}/{y}.png?sql=SELECT * FROM bc_seagrass WHERE toggle = true AND (action <> \'delete\' OR action IS NULL)'},
+        {display_name: 'Sabkha', tiles_url_format: 'https://carbon-tool.cartodb.com/tiles/bc_sabkha/{z}/{x}/{y}.png?sql=SELECT * FROM bc_sabkha WHERE toggle = true AND (action <> \'delete\' OR action IS NULL)'},
+        {display_name: 'Salt Marsh', tiles_url_format: 'https://carbon-tool.cartodb.com/tiles/bc_saltmarsh/{z}/{x}/{y}.png?sql=SELECT * FROM bc_saltmarsh WHERE toggle = true AND (action <> \'delete\' OR action IS NULL)'},
+        {display_name: 'Algal Mat', tiles_url_format: 'https://carbon-tool.cartodb.com/tiles/bc_algal_mat/{z}/{x}/{y}.png?sql=SELECT * FROM bc_algal_mat WHERE toggle = true AND (action <> \'delete\' OR action IS NULL)'},
+        {display_name: 'Other', tiles_url_format: 'https://carbon-tool.cartodb.com/tiles/bc_other/{z}/{x}/{y}.png?sql=SELECT * FROM bc_other WHERE toggle = true AND (action <> \'delete\' OR action IS NULL)'}
+      ])
     end
   end
 
@@ -11,7 +21,7 @@ describe BlueCarbonLayer do
     before :all do
       @result = BlueCarbonLayer.get_operations
     end
-    it "should return sum only" do
+    it "returns the sum only" do
       @result.should match_array [{
         name: "sum",
         display_name: "Sum"
