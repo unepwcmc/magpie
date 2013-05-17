2be2f31a081f6a93b26566f5cfa29c5956ef2d4c
Decio Battaglia <decio.battaglia@gmail.com>
2013-05-17 14:10:51 +0100

    fixed cartodb queries for total_area

--- lib/modules/carbon_query.rb
+++ lib/modules/carbon_query.rb
@@ -16,11 +16,10 @@ class CarbonQuery
       SQL
   end
 
-  def self.total_area(the_geom, table_name)
-
+  def self.total_intersection_area(the_geom, table_name)
       <<-SQL
       SELECT SUM(area) as area FROM
-        (SELECT ST_AREA(ST_Transform(ST_SetSRID(ST_INTERSECTION(b.the_geom, a.the_geom), 4326),27040))/1000 as area
+        (SELECT ST_AREA(ST_Transform(ST_SetSRID(ST_INTERSECTION(b.the_geom, a.the_geom), 4326),27040))/100000 as area
         FROM #{table_name} a 
         INNER JOIN 
           (SELECT
@@ -32,6 +31,12 @@ class CarbonQuery
         ON ST_Intersects(a.the_geom, b.the_geom)) c;
       SQL
   end
+
+  def self.total_area(the_geom, table_name)
+     <<-SQL
+       SELECT ST_AREA(ST_Transform(ST_SetSRID(#{the_geom}, 4326),27040))/100000 as area
+       SQL
+    end
 
   def self.habitat(the_geom, table_name)
 
