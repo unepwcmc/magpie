3140b1d897206dc5389a094de9ba50e0860f64ed
James Cox <th3james@gmail.com>
2013-05-22 17:10:53 +0100

    Only request data standard for protected areas

--- lib/modules/protected_planet_layer_operations/protected_planet_layer_operations.rb
+++ lib/modules/protected_planet_layer_operations/protected_planet_layer_operations.rb
@@ -1,7 +1,12 @@
 module ProtectedPlanetLayerOperations
+  QUERY_FILTERS = {
+    skip_carbon_stats: true,
+    only_data_standard: true
+  }.to_json
+
   def self.query_protected_planet(area_of_interest)
     if area_of_interest.polygons.count > 0
-      response = RestClient.post("http://protectedplanet.net/api2/geo_searches", "data=#{area_of_interest.to_wkt.to_json}&filters=#{{skip_carbon_stats: true}.to_json}")
+      response = RestClient.post("http://protectedplanet.net/api2/geo_searches", "data=#{area_of_interest.to_wkt.to_json}&filters=#{QUERY_FILTERS}")
       response_json = JSON.parse(response)
 
       if response_json['error']
