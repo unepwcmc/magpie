cf107209f2694c1503a02ed28f5ae51b06bcf9b2
James Cox <th3james@gmail.com>
2013-05-30 09:11:49 +0100

    Attempt to fix travis build by removing cartodb dep

--- config/initializers/cartodb.rb
+++ config/initializers/cartodb.rb
@@ -1,1 +1,3 @@
-CARTODB_CONFIG = YAML.load_file(Rails.root.join('config/cartodb_config.yml'))
+unless Rails.env == 'test'
+  CARTODB_CONFIG = YAML.load_file(Rails.root.join('config/cartodb_config.yml'))
+end
