7c1cd52439d41eb88c37b3537ef3c62a6e427dde
Adam Mulligan <adammulligan@gmail.com>
2013-05-30 10:58:16 +0100

    Cache results for protected planet 😻

--- lib/modules/protected_planet_layer_operations/protected_planet_layer_operations.rb
+++ lib/modules/protected_planet_layer_operations/protected_planet_layer_operations.rb
@@ -6,14 +6,19 @@ module ProtectedPlanetLayerOperations
 
   def self.query_protected_planet(area_of_interest)
     if area_of_interest.polygons.count > 0
-      response = RestClient.post("http://protectedplanet.net/api2/geo_searches", "data=#{area_of_interest.to_wkt.to_json}&filters=#{QUERY_FILTERS}")
-      response_json = JSON.parse(response)
+      key = "aoi-#{area_of_interest.id}-#{area_of_interest.most_recent_polygon_updated_at}"
 
-      if response_json['error']
-        raise TimeoutError, 'Area was too large to calculate.'
+      return Rails.cache.fetch(key) do
+        response = RestClient.post("http://protectedplanet.net/api2/geo_searches", "data=#{area_of_interest.to_wkt.to_json}&filters=#{QUERY_FILTERS}")
+        response_json = JSON.parse(response)
+
+        if response_json['error']
+          Rails.cache.delete(key)
+          raise TimeoutError, 'Area was too large to calculate.'
+        end
+
+        response_json
       end
-
-      return response_json
     end
   end
 end
