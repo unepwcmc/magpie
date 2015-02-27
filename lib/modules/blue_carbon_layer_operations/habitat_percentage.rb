eac560d379488cba0e206e184d4d6372560f0cfe
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2015-02-27 14:59:38 +0000

    adapt habitat_percentage to class

--- lib/modules/blue_carbon_layer_operations/habitat_percentage.rb
+++ lib/modules/blue_carbon_layer_operations/habitat_percentage.rb
@@ -1,14 +1,8 @@
-module BlueCarbonLayerOperations::HabitatPercentage
+class BlueCarbonLayerOperations::HabitatPercentage < BlueCarbonLayerOperations::Base
   Name = 'habitat_percentage'
   DisplayName = 'Percentage of Habitats Selected'
 
-  def self.perform(area_of_interest)
-    geoms = []
-    area_of_interest.polygons_as_geo_json_polygons.each do |polygon|
-      geoms << "ST_GeomFromGeoJSON('#{polygon.to_json}')"
-    end
-
-    response = BlueCarbonLayerOperations.cartodb_query(:global_percent_area, geoms)
-    return response.to_json
+  def perform
+    results_for(:global_percent_area).to_json
   end
 end
