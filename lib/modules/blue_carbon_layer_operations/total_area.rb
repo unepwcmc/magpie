615cd5dd654df84e22f90f27348f7a693cdd048d
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2015-02-27 15:00:39 +0000

    adapt total_area to class

--- lib/modules/blue_carbon_layer_operations/total_area.rb
+++ lib/modules/blue_carbon_layer_operations/total_area.rb
@@ -1,19 +1,8 @@
-module BlueCarbonLayerOperations::TotalArea
+class BlueCarbonLayerOperations::TotalArea < BlueCarbonLayerOperations::Base
   Name = 'total_area'
   DisplayName = 'Total Area'
 
-  def self.perform(area_of_interest)
-    geoms = []
-    area_of_interest.polygons_as_geo_json_polygons.each do |polygon|
-      geoms << "ST_GeomFromGeoJSON('#{polygon.to_json}')"
-    end
-
-    response = BlueCarbonLayerOperations.cartodb_query(:total_area, geoms)
-
-    if response.length > 0
-      return response[0]["area"]
-    end
-
-    return 0
+  def perform
+    results_for(:total_area).first.try(:[], 'area') || 0
   end
 end
