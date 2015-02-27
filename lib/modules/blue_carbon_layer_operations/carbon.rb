8839cecc28f6773c8e07132dfdb402b22438daec
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2015-02-27 14:59:01 +0000

    adapt carbon operation to class

--- lib/modules/blue_carbon_layer_operations/carbon.rb
+++ lib/modules/blue_carbon_layer_operations/carbon.rb
@@ -1,14 +1,8 @@
-module BlueCarbonLayerOperations::Carbon
+class BlueCarbonLayerOperations::Carbon < BlueCarbonLayerOperations::Base
   Name = 'carbon'
   DisplayName = 'Carbon'
 
-  def self.perform(area_of_interest)
-    geoms = []
-    area_of_interest.polygons_as_geo_json_polygons.each do |polygon|
-      geoms << "ST_GeomFromGeoJSON('#{polygon.to_json}')"
-    end
-
-    response = BlueCarbonLayerOperations.cartodb_query(:habitat, geoms)
-    return response.to_json
+  def perform
+    results_for(:habitat)
   end
 end
