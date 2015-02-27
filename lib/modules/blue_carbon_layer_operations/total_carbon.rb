a54a6ed042d38665fe8f811acb9a5eb07db930ab
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2015-02-27 15:00:48 +0000

    adapt total_carbon to class

--- lib/modules/blue_carbon_layer_operations/total_carbon.rb
+++ lib/modules/blue_carbon_layer_operations/total_carbon.rb
@@ -1,18 +1,8 @@
-module BlueCarbonLayerOperations::TotalCarbon
+class BlueCarbonLayerOperations::TotalCarbon < BlueCarbonLayerOperations::Base
   Name = 'total_carbon'
   DisplayName = 'Total Carbon'
 
-  def self.perform area_of_interest
-    geoms = area_of_interest.polygons_as_geo_json_polygons.map do |polygon|
-      "ST_GeomFromGeoJSON('#{polygon.to_json}')"
-    end
-
-    the_geom = ::Utils.st_makevalid(::Utils.st_union(geoms))
-    carbon_view_name = "blueforest_carbon_#{Rails.env}_#{area_of_interest.properties['country']}"
-
-    query = ::Utils.render_query(:total_carbon, binding)
-    response = ::Utils.query_cartodb(query)
-
-    response.length > 0 ? response[0]["carbon"] : 0
+  def perform
+    results_for(:total_carbon).first.try(:[], 'carbon') || 0
   end
 end
