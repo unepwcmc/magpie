fae64b7dcdeab6fa98c13765045b3fdbda33439c
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2015-02-27 14:58:19 +0000

    adapt area_km2 to class

--- lib/modules/blue_carbon_layer_operations/area_km2.rb
+++ lib/modules/blue_carbon_layer_operations/area_km2.rb
@@ -1,19 +1,8 @@
-module BlueCarbonLayerOperations::AreaKm2
+class BlueCarbonLayerOperations::AreaKm2 < BlueCarbonLayerOperations::Base
   Name = 'area_km2'
   DisplayName = 'Area'
 
-  def self.perform(area_of_interest)
-    geoms = []
-    area_of_interest.polygons_as_geo_json_polygons.each do |polygon|
-      geoms << "ST_GeomFromGeoJSON('#{polygon.to_json}')"
-    end
-
-    the_geom = ::Utils.st_makevalid(::Utils.st_union(geoms))
-    country_name = area_of_interest.properties['country']
-
-    query = ::Utils.render_query(:area_km2, binding)
-    response = ::Utils.query_cartodb(query)
-
-    return response.to_json
+  def perform
+    results_for(:area_km2).to_json
   end
 end
