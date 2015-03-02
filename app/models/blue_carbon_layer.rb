8a883f5de8796e55eeb0100959ce64417ea747a5
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2015-03-02 15:19:01 +0000

    add methods to BlueCarbonLayer to pass info to CarbonOperations

--- app/models/blue_carbon_layer.rb
+++ app/models/blue_carbon_layer.rb
@@ -1,2 +1,17 @@
 class BlueCarbonLayer < ProjectLayer
+  def self.get_operations
+    super('carbon_operations')
+  end
+
+  def self.fetch_result(operation, area_of_interest)
+    "CarbonOperations::#{operation.to_s.classify}".constantize.perform(self, area_of_interest)
+  end
+
+  def self.carbon_view _
+    "bc_carbon_#{Rails.env}"
+  end
+
+  def self.habitat_view habitat, _
+    "bc_#{habitat}_#{Rails.env}"
+  end
 end
